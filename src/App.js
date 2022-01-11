8d8f82d80856f141f2478cdee29de008a4832880
Ayush6501 <ayushmajumdar6501@gmail.com>
2022-01-11 19:25:24 +0530

    Added Routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import AboutPage from "./components/AboutPage/AboutPage"
 import Landing from "./components/Landing/Landing"
 import LoginPage from "./components/LoginPage/LoginPage"
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <>
-      {/*<AboutPage/>*/}
-      {/*<Landing/>*/}
-      <LoginPage/>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Landing/>}/>
+                <Route
+                    path="/login"
+                    element={<LoginPage />}
+                />
+                <Route path="/about" element={<AboutPage />}></Route>
+            </Routes>
+        </BrowserRouter>
     </>
   );
 }
