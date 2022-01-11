e4d5d6ad3e57202d96dad45549ffb41d37978aed
Siddharth Sarma <siddharthgsarma5@gmail.com>
2022-01-11 23:45:23 +0530

    merged

--- src/App.js
+++ src/App.js
@@ -1,32 +1,20 @@
-import AboutPage from "./components/AboutPage/AboutPage"
-import Landing from "./components/Landing/Landing"
-import LoginPage from "./components/LoginPage/LoginPage"
-<<<<<<< HEAD
-import Designation from "./Designation";
-=======
-import {BrowserRouter, Routes, Route} from "react-router-dom";
->>>>>>> 8d8f82d80856f141f2478cdee29de008a4832880
-
+import AboutPage from "./components/AboutPage/AboutPage";
+import Landing from "./components/Landing/Landing";
+import LoginPage from "./components/LoginPage/LoginPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      <AboutPage/>
-      {/* <Designation /> */}
+      <AboutPage />
       {/* <Landing/> */}
       {/* <LoginPage/> */}
-=======
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Landing/>}/>
-                <Route
-                    path="/login"
-                    element={<LoginPage />}
-                />
-                <Route path="/about" element={<AboutPage />}></Route>
-            </Routes>
-        </BrowserRouter>
->>>>>>> 8d8f82d80856f141f2478cdee29de008a4832880
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/about" element={<AboutPage />}></Route>
+        </Routes>
+      </BrowserRouter>{" "}
     </>
   );
 }
