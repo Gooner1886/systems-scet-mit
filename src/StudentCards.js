2eec84fb718cd6c82f6ebbe2248b9d354c362816
Your Name <you@example.com>
2022-01-14 16:56:06 +0530

    commit

---
+++ src/StudentCards.js
@@ -0,0 +1,15 @@
+import { Box, Text,Stack,Badge,Image,Button } from '@chakra-ui/react'
+import React from 'react'
+import WithSubnavigation from './components/Navbar/Navbar'
+
+function StudentCards() {
+    return (
+        <>
+        <WithSubnavigation/>
+        
+        
+      </>
+    );
+}
+
+export default StudentCards;
